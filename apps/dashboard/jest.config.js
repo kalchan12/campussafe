7c29f29fc85b120d89db69963a83bb175175aceb
kalchan12 <kalpsycho51@gmail.com>
2026-08-26 22:42:55 +0300

    chore(dashboard): remove tracked build artifacts and configure jest

---
+++ apps/dashboard/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+        module: 'commonjs',
+      },
+    }],
+  },
+};
