7e4c31da931f00a48110f04fca6f9d58bea4370b
kalchan12 <kalpsycho51@gmail.com>
2026-09-01 11:18:06 +0300

    fix(mobile/android): enable core library desugaring and multidex for flutter_local_notifications

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -27,6 +28,7 @@ android {
         // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -38,6 +40,10 @@ android {
     }
 }
 
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
@@ -47,3 +53,4 @@ kotlin {
 flutter {
     source = "../.."
 }
+
